2b34dfd4de83a9eb12f2edde8b19026c0ac61732
Jaafar Osamah <jaafarosamah@Jaafars-MacBook-Pro.local>
2022-06-29 13:32:20 +0200

    Edit

--- pass-gen.php
+++ pass-gen.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+<?php
 if (isset($_POST['generate'])) {
     $uppr_case = "ABCDEFGHIJKLMNOPQRSTUWXYZ";
     $lower_case = "abcdefghijklmnopqrstuwxyz";
